51a785c0780cd0bbf0b7706178cac236e90621d5
Bitros Wang <mopspecial@gmail.com>
2025-07-21 09:12:30 +0000

    dev update

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
 python -m pip install -U pip poetry
-poetry install
-poetry env activate
 
+poetry install --no-root
+poetry run pre-commit autoupdate
+poetry run pre-commit install
+poetry run pre-commit install-hooks
 
 code --uninstall-extension vscjava.vscode-java-pack
