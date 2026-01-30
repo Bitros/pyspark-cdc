d94834509128e236bac2e3e0db8f835ac82e62de
Bitros Wang <mopspecial@gmail.com>
2026-01-30 09:13:34 +0000

    isolate test table name
    Signed-off-by: Bitros Wang <mopspecial@gmail.com>

--- tests/pyspark_cdc_test/bugs/test_bugs_found.py
+++ tests/pyspark_cdc_test/bugs/test_bugs_found.py
@@ -4,6 +4,7 @@ from typing import TYPE_CHECKING
 
 import pytest
 
+from pyspark_cdc_test import catalog_schema
 from pyspark_cdc_test.utils.employee_generator import EmployeeGenerator
 
 if TYPE_CHECKING:
@@ -18,19 +19,21 @@ def mock_df(mock_spark: SparkSession) -> DataFrame:
 
 @pytest.mark.xfail(reason="Known bug in https://github.com/delta-io/delta/issues/4823")
 def test_bug_01(mock_spark: SparkSession, mock_df: DataFrame) -> None:
-    mock_df.writeTo("employee").using("delta").clusterBy("country", "gender").create()
+    mock_df.writeTo(f"{catalog_schema}.bug01_employee").using("delta").clusterBy(
+        "country", "gender"
+    ).create()
 
 
 @pytest.mark.xfail(reason="Known bug in https://github.com/delta-io/delta/issues/4855")
 def test_bug_02(mock_spark: SparkSession, mock_df: DataFrame) -> None:
-    mock_df.writeTo("employee").using("delta").partitionedBy(
+    mock_df.writeTo(f"{catalog_schema}.bug02_employee").using("delta").partitionedBy(
         "COUNTRY", "GENDER"
     ).option("overwriteSchema", True).createOrReplace()
 
-    mock_df.writeTo("employee").using("delta").partitionedBy(
+    mock_df.writeTo(f"{catalog_schema}.bug02_employee").using("delta").partitionedBy(
         "COUNTRY", "GENDER"
     ).option("overwriteSchema", True).createOrReplace()
 
-    mock_df.writeTo("employee").using("delta").partitionedBy(
+    mock_df.writeTo(f"{catalog_schema}.bug02_employee").using("delta").partitionedBy(
         "COUNTRY", "GENDER"
     ).option("overwriteSchema", True).createOrReplace()
