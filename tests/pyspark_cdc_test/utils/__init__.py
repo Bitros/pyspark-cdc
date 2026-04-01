9d846e01e253fa14b101564acab59fa6b4fe6ba3
Bitros Wang <mopspecial@gmail.com>
2026-04-01 12:25:57 +0000

    unique table name

--- tests/pyspark_cdc_test/utils/__init__.py
+++ tests/pyspark_cdc_test/utils/__init__.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import hashlib
 import sys
 
 
 def generate_table_name() -> str:
     caller_frame = sys._getframe(1)
-    file_name = caller_frame.f_code.co_filename.removesuffix(".py").split("/")[-1]
+    full_file_name = caller_frame.f_code.co_filename
+    file_name = full_file_name.removesuffix(".py").split("/")[-1]
     func_name = caller_frame.f_code.co_qualname
-    return f"{file_name}_{func_name}"
+    return f"{file_name}_{func_name}_{hashlib.md5(full_file_name.encode()).hexdigest()[:8]}"
